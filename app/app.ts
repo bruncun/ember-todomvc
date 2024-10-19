af18d2b59f22997e477e397121119d9528bfe75f
Bruno Cunha <bruncun@icloud.com>
2024-10-19 13:46:02 -0400

    remove unnecessary page title logic

--- app/app.ts
+++ app/app.ts
@@ -3,7 +3,6 @@ import Application from '@ember/application';
 import Resolver from 'ember-resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from 'ember-todomvc/config/environment';
-import type PageTitle from 'ember-page-title/helpers/page-title';
 
 export default class App extends Application {
   modulePrefix = config.modulePrefix;
@@ -12,9 +11,3 @@ export default class App extends Application {
 }
 
 loadInitializers(App, config.modulePrefix);
-
-declare module '@glint/environment-ember-loose/registry' {
-  export default interface Registry {
-    'page-title': typeof PageTitle;
-  }
-}
